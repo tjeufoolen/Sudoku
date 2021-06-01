97b82b2973a2abd7f371f2081a51a61ea1bab048
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2021-06-01 15:09:43 +0200

    Created modal and refactored sidebar

---
+++ DP1_Sudoku/DP1_Sudoku/Shared/AddGameModal.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DP1_Sudoku.Shared
+{
+    public partial class AddGameModal
+    {
+        public string ModalIdentifier = "addGameModal";
+        public Modal Modal;
+
+        [Parameter]
+        public EventCallback<int> OnAddGameCompleted { get; set; }
+
+        public async void AddGame()
+        {
+            // TODO: Add game implementation
+
+            await OnAddGameCompleted.InvokeAsync(1);
+        }
+    }
+}
---
+++ DP1_Sudoku/DP1_Sudoku/Shared/Modal.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DP1_Sudoku.Shared
+{
+    public partial class Modal
+    {
+        [Parameter] public string Identifier { get; set; }
+        [Parameter] public EventCallback<string> IdentifierChanged { get; set; }
+
+        public string LabelIdentifier { get => $"{Identifier}Label"; }
+
+        [Parameter]
+        public RenderFragment Title { get; set; }
+
+        [Parameter]
+        public RenderFragment Body { get; set; }
+
+        [Parameter]
+        public RenderFragment Footer { get; set; }
+
+    }
+}
