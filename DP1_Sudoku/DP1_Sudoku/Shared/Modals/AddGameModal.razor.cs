079a4cf08c837082b31b5d4e41357d7f3325ae16
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2021-06-01 16:50:06 +0200

    File validation

---
+++ DP1_Sudoku/DP1_Sudoku/Shared/Modals/AddGameModal.razor.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DP1_Sudoku.Shared.Modals
+{
+    public partial class AddGameModal
+    {
+        [Parameter] public EventCallback<int> OnAddGameCompleted { get; set; }
+
+        public string ModalIdentifier = "addGameModal";
+        public Modal Modal;
+        public AddGameFormModel FormModel = new();
+
+        public void OnInputFileChange(InputFileChangeEventArgs e)
+        {
+            this.FormModel.File = e.File;
+        }
+
+        public async void AddGame()
+        {
+            // TODO: Add game implementation
+            Console.WriteLine(FormModel.Name);
+            Console.WriteLine(FormModel.Type);
+            // ----
+
+            await OnAddGameCompleted.InvokeAsync(1);
+        }
+    }
+
+    public class AddGameFormModel
+    {
+        [StringLength(25, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression("4x4|6x6|9x9|samurai|jigsaw", ErrorMessage = "Incorrect type.")]
+        public string Type { get; set; } = "";
+
+        [Required]
+        [FileValidation]
+        public IBrowserFile File { get; set; }
+    }
+
+    public class FileValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            AddGameFormModel _model = validationContext.ObjectInstance as AddGameFormModel;
+
+            if (_model.File.Size > 1000000) //TODO: Termine what the limit should be.
+            {
+                return new ValidationResult("This file is too powerfull, the max filesize is 1MB.", new[] { validationContext.MemberName });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
