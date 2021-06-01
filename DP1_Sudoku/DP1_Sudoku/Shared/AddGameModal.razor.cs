b29cb6740d7e4b22904eaea4f364102a41eb0e79
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2021-06-01 15:56:29 +0200

    Custom validation and basic form fields

--- DP1_Sudoku/DP1_Sudoku/Shared/AddGameModal.razor.cs
+++ DP1_Sudoku/DP1_Sudoku/Shared/AddGameModal.razor.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DP1_Sudoku.Shared
 {
@@ -7,14 +9,29 @@ namespace DP1_Sudoku.Shared
         public string ModalIdentifier = "addGameModal";
         public Modal Modal;
 
+        public AddGameFormModel FormModel = new();
+
         [Parameter]
         public EventCallback<int> OnAddGameCompleted { get; set; }
 
         public async void AddGame()
         {
             // TODO: Add game implementation
+            Console.WriteLine(FormModel.Name);
+            Console.WriteLine(FormModel.Type);
+            // ----
 
             await OnAddGameCompleted.InvokeAsync(1);
         }
+    }
+
+    public class AddGameFormModel
+    {
+        [StringLength(25, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression("4x4|6x6|9x9|samurai|jigsaw", ErrorMessage = "Incorrect type")]
+        public string Type { get; set; } = "";
     }
 }
---
+++ DP1_Sudoku/DP1_Sudoku/Shared/CustomValidation.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Linq.Expressions;
+
+namespace DP1_Sudoku.Shared
+{
+    public partial class CustomValidation<TValue> : ComponentBase
+    {
+        [CascadingParameter] private EditContext EditContext { get; set; }
+
+        [Parameter] public Expression<Func<TValue>> For { get; set; }
+        [Parameter] public string Class { get; set; }
+
+        private FieldIdentifier _fieldIdentifier;
+
+        protected override void OnInitialized()
+        {
+            _fieldIdentifier = FieldIdentifier.Create(For);
+            EditContext.OnValidationStateChanged += HandleValidationStateChanged;
+        }
+
+        private void HandleValidationStateChanged(object o, ValidationStateChangedEventArgs args) => StateHasChanged();
+
+        public void Dispose()
+        {
+            EditContext.OnValidationStateChanged -= HandleValidationStateChanged;
+        }
+    }
+}
