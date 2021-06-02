71cf525b025220b2267d96157666aa8cd6500811
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2021-06-02 17:14:31 +0200

    Feature/frontend setup (#3)
    * removed old files
    
    * scss for components
    
    * css setup with hot reload and scss compiling
    
    * Created basic layout with navbar and sidebar
    
    * Update Sidebar.razor
    
    * Created modal and refactored sidebar
    
    * inbetween
    
    * Custom validation and basic form fields
    
    * File validation
    
    * inbetween
    
    * Moved files to its own directories

---
+++ DP1_Sudoku/DP1_Sudoku/Shared/Sidebar.razor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace DP1_Sudoku.Shared
+{
+    public partial class Sidebar
+    {
+        #region Visibility
+        private bool _visible = true;
+        public bool Visible
+        {
+            get { return _visible; }
+            set
+            {
+                _visible = value;
+                InvokeAsync(StateHasChanged);
+            }
+        }
+
+        public string VisibilityClassList => Visible ? "visible" : "";
+        #endregion Visibility
+
+        // TODO: Replace with actual gametype viewmodels which contain all gamefiles per type.
+        private readonly List<string> _gameTypes = new() { "4x4", "6x6", "9x9", "samurai", "jigsaw" };
+    }
+}
