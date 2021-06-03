f3bef342fc165a999bd7aefb11bca9d246e46822
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2021-06-03 17:36:35 +0200

    created nunit 3 project for businesslogic testing (#9)

---
+++ DP1_Sudoku/DP1_Sudoku.Tests/ExampleTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace DP1_Sudoku.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Example()
+        {
+            Assert.Pass();
+        }
+    }
+}
